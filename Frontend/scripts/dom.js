63da9e29727ba7c0e2b9149bcbbe8b6ed4e35065
hilario <hreddel@pioix.edu.ar>
2026-06-25 10:04:57 -0300

    agregue dom

---
+++ Frontend/scripts/dom.js
@@ -0,0 +1,11 @@
+function getNombreDeJugador() {
+    return document.getElementById("ingresoJugadorNuevo").value
+}
+
+function getCantidadDePartidos() {
+    return document.getElementById("ingresoCantidadDePartidos").value
+}
+
+function getPosicionEnLaCancha() {
+    return document.getElementById("ingresoPosicionEnLaCancha").value
+}
--- Frontend/scripts/logic.js
+++ Frontend/scripts/logic.js
@@ -37,3 +37,80 @@ async function loginUsuario() {
     alert(resultado.res);
   }
 }
+
+
+/*
+
+// 1
+function tomarDatos() {
+  let datos = {
+    nombre_completo: getNombreDeEquipo(),
+    partidos_totales: getCantidadDeSocios(),
+    posicion_en_la_cancha: getCantidadDeJugadores(),
+  };
+
+  llamadoAlPost(datos);
+}
+
+async function llamadoAlPost(datos) {
+  //Mando los datos al BACKEND
+  const response = await fetch("http://localhost:4000/adddata", {
+    method: "POST", //GET, POST, PUT o DELETE
+    headers: {
+      //Va siempre igual, le aclaro que la informacion va a viajar como JSON
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(datos), //JSON.stringify convierte de objeto a JSON
+  });
+  // --Me quedo esperando--
+
+  //En response tengo la respuesta del BACKEND
+  console.log(response); //Imprimo el json
+  let result = await response.json();
+  console.log(result);
+}
+
+
+
+
+
+
+async function cargarEquipos() {
+  const response = await fetch("http://localhost:4000/");
+  const equipos = await response.json();
+  console.log(equipos);
+
+  let contenido = "";
+
+  equipos.forEach(function (equipo) {
+    contenido += `<option id="${equipo.nombre_de_equipo}" value="${equipo.nombre_de_equipo}">${equipo.nombre_de_equipo}</option>`;
+  });
+  document.getElementById("selectEquipos").innerHTML = contenido;
+}
+cargarEquipos();
+
+async function llamadoAlDelete() {
+  //Mando los datos al BACKEND
+  //   let nombre = document.getElementById("selectEquipos").value;
+
+  let datos = {
+    nombre_de_equipo: document.getElementById("selectEquipos").value,
+  };
+
+  console.log("Datos: ", datos);
+  const response = await fetch("http://localhost:4000/equiposBorrar", {
+    method: "DELETE", //GET, POST, PUT o DELETE
+    headers: {
+      //Va siempre igual, le aclaro que la informacion va a viajar como JSON
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(datos), //JSON.stringify convierte de objeto a JSON
+  });
+  // --Me quedo esperando--
+
+  //En response tengo la respuesta del BACKEND
+    let result = await response.json();
+    console.log(result)
+  cargarEquipos();
+}
+*/
